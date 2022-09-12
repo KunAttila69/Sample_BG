332adf7fbdef87ccda6267513cb9d87135cddf4e
KunAttila69 <71005372+KunAttila69@users.noreply.github.com>
2022-09-12 19:35:26 +0200

    Add files via upload

--- stars.js
+++ stars.js
@@ -2,8 +2,8 @@ class Star{
   constructor(x,y,size,speed,dir){
     this.x = x,
     this.y = y,
-    this.size = size
-    this.speed = speed
+    this.size = size,
+    this.speed = speed,
     this.dir = dir
   }
 }
@@ -11,9 +11,38 @@ function CreateStars(stars,starCount, maxWidth, maxHeight){
   for (let i = 0; i < starCount; i++) {
     let starX = Math.floor(Math.random()*maxWidth)-10;
     let starY = Math.floor(Math.random()*maxHeight)-10;
-    let starSize = Math.floor(Math.random()*5)+3;
-    let starSpeed = Math.floor(Math.random()*4)+1;
-    let starDir = (Math.random()*0.02)-0.1;
+    let starSize = Math.floor(Math.random()*2)+2;
+    let starSpeed = Math.floor(Math.random()*3)+1;
+    let starDir = (Math.random()*0.02)-0.01;
     stars.push(new Star(starX,starY,starSize,starSpeed,starDir));
+  }
+}
+function DrawStar(star){
+  ctx.beginPath();
+  ctx.arc(star.x, star.y, star.size*star.speed, 0, 2 * Math.PI);
+  //Set color
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.stroke();
+}
+function MoveStar(star) {
+  //X coordinate
+  if(star.x > canvas.width+5){
+      star.x = -1;
+  }
+  else if(star.x < -5){
+      star.x = canvas.width-1;
+  }
+  else{
+      star.x += star.speed*star.dir;
+  }
+  star.dir += (Math.random()*0.02)-0.01;
+
+  //Y coordinate
+  if (star.y < canvas.height+5) {
+      star.y += star.speed;
+  }
+  else{
+      star.y = -5;
   }
 }
